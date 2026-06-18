36a40f2fca1a316271d0bcc7e73bb2f1c4e194a5
root <root@syria-platform.org>
2026-06-18 11:29:30 +0000

    🛡️ حارس السيرفر - خدمة 24/7

--- bayan_wasm/target/release/build/wasm-bindgen-shared-3d88751e091264b3/build_script_build-3d88751e091264b3.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/al-bayan-compiler/bayan_wasm/target/release/build/wasm-bindgen-shared-3d88751e091264b3/build_script_build-3d88751e091264b3.d: /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/wasm-bindgen-shared-0.2.125/build.rs
-
-/data/data/com.termux/files/home/al-bayan-compiler/bayan_wasm/target/release/build/wasm-bindgen-shared-3d88751e091264b3/build_script_build-3d88751e091264b3: /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/wasm-bindgen-shared-0.2.125/build.rs
-
-/data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/wasm-bindgen-shared-0.2.125/build.rs:
--- bayan_wasm/target/release/deps/bumpalo-457be53b459ec5e8.d
+++
@@ -1,9 +0,0 @@
-/data/data/com.termux/files/home/al-bayan-compiler/bayan_wasm/target/release/deps/bumpalo-457be53b459ec5e8.d: /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/lib.rs /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/alloc.rs /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/../README.md
-
-/data/data/com.termux/files/home/al-bayan-compiler/bayan_wasm/target/release/deps/libbumpalo-457be53b459ec5e8.rlib: /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/lib.rs /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/alloc.rs /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/../README.md
-
-/data/data/com.termux/files/home/al-bayan-compiler/bayan_wasm/target/release/deps/libbumpalo-457be53b459ec5e8.rmeta: /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/lib.rs /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/alloc.rs /data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/../README.md
-
-/data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/lib.rs:
-/data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/alloc.rs:
-/data/data/com.termux/files/home/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bumpalo-3.20.3/src/../README.md:
